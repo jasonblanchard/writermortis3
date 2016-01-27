e927d628ce4c77b8856c32c2db19c079158895f7
Jason Blanchard <jas.blanchard@gmail.com>
2016-01-26 20:27:13 -0500

    Removed unused code

--- app/reducers/reducer.js
+++ app/reducers/reducer.js
@@ -2,20 +2,6 @@ import actionConstants from 'app/actions/constants';
 import { combineReducers } from 'redux';
 import Immutable from 'immutable';
 
-// function story(state = Immutable.fromJS({}), action) {
-//   switch (action.type) {
-//     case actionConstants.LOAD_STORY:
-//       return action.story;
-//     case actionConstants.ADD_NEW_PIECE:
-//       const currentStory = state;
-//       let pieces = currentStory.get('pieces');
-//       pieces = pieces.push(action.piece);
-//       return currentStory.set('pieces', pieces);
-//     default:
-//       return state;
-//   }
-// }
-
 function currentStoryId(state = null, action) {
   switch (action.type) {
     case actionConstants.LOAD_CURRENT_STORY:
